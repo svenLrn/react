73d98b6d7aa6e953b61ba71704001178bfeaa064
Sven <chanck.mds@gmail.com>
2023-10-08 23:57:45 +0200

    ex 06 is done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,1 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const sumOf = (a,b) =>{
-    const sum = (a+b)
-    return <h2>{sum}</h2>
-  }
-
-  return (
-    <>
-      {sumOf(2,2)}
-    </>
-  )
-}
-
-export default App
