ef881a92ff7065d972a480291a9a2b41b8131ed2
Sven <chanck.mds@gmail.com>
2023-10-12 14:26:49 +0200

    ex 12 is done

--- my-app/src/HelloWorld.jsx
+++ my-app/src/HelloWorld.jsx
@@ -1,8 +1,10 @@
+import { Message } from "./Message"
+
 export const HelloWorld = () =>{
     return (
     <>
         <h2>Hello, World!</h2>
-        <p>What a beautiful day!</p>
+        <Message/>
     </>
     )
 }
