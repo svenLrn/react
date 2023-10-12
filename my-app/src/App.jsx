2816ada867e66c23788b8784f05b6e694c93a5c3
Sven <chanck.mds@gmail.com>
2023-10-12 14:34:40 +0200

    ex 15 is done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,12 @@
 import { HelloWorld } from "./HelloWorld";
+import { Message } from "./Message";
 
 export function App(){
     return(
         <div>
             <HelloWorld/>
+            <HelloWorld/>
+            <Message/>
         </div>
     )
 }
