e2b623d3426f51f22f36339159963ac1b1379aa9
Sven <chanck.mds@gmail.com>
2023-10-24 23:17:50 +0200

    ex 23 is done

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -3,7 +3,7 @@ export const Welcome = ({name, age}) =>{
     return (
     <>
         <p>Welcome, {name}!</p>
-        {age >= 18 && <Age age = {age}/>}
+        {age >= 18 && age<=65 && <Age age = {age}/>}
     </>
     )
 }
