f5977f5f771c807e607b1c7d2ccf8d58973ad91a
Sven <chanck.mds@gmail.com>
2023-10-24 03:17:22 +0200

    ex 18 is done

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -1,7 +1,8 @@
-export const Welcome = ({name}) =>{
+export const Welcome = ({name = 'Sven', age}) =>{
     return (
     <>
         <p>Welcome, {name}!</p>
+        <p>Your age is {age}</p>
     </>
     )
 }
