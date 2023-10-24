7e3547c8f8c1220c175da6881e1fd360e22bd5ff
Sven <chanck.mds@gmail.com>
2023-10-24 23:38:19 +0200

    ex 24 is done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,7 @@ import { Welcome } from "./Welcome";
 export function App(){
     return(
         <div>
-            <Welcome name = {<strong>Sven</strong>} age = '66'/>
+            <Welcome name = 'John' age = '19'/>
         </div>
     )
 }
--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -2,8 +2,9 @@ import { Age } from "./Age";
 export const Welcome = ({name, age}) =>{
     return (
     <>
-        <p>Welcome, {name}!</p>
-        {age >= 18 && age<=65 && <Age age = {age}/>}
+        <p>Welcome, <strong>{name}</strong>!</p>
+        
+        {age >= 18 && age<=65 && name == 'John' ? <Age age = {age}/> : null}
     </>
     )
 }
