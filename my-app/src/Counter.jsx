2141ba475bfe48607f5b8a3df3e6be669ab71c9f
Sven <chanck.mds@gmail.com>
2023-10-27 04:40:10 +0200

    ex 33 is donee

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -6,10 +6,19 @@ export function Counter({initialValue = 0, increment = 1}) {
     function handleCount() {
         setCounter((c)=> c+increment);
     }
+    function handleDecrement() {
+        setCounter((c)=> c-increment);
+    }
+    function resetCount() {
+        setCounter(initialValue);
+    }
     return (
         <div>
             <CounterDisplay counter = {counter}/>
-            <button onClick={handleCount}>+1</button>
+            <button onClick={handleCount}>+{increment}</button>
+            <button onClick={handleDecrement}>-{increment}</button>
+            <button onClick={resetCount}>to {initialValue}</button>
+
         </div>
     )
 }
