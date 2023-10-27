ea66047f439512c253015ccbd5d7208b91b57562
Sven <chanck.mds@gmail.com>
2023-10-27 04:42:03 +0200

    ex 32 upd

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -9,7 +9,7 @@ export function Counter({initialValue = 0, increment = 1}) {
     return (
         <div>
             <CounterDisplay counter = {counter}/>
-            <button onClick={handleCount}>+1</button>
+            <button onClick={handleCount}>+{increment}</button>
         </div>
     )
 }
