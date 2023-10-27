6005e2c29f5c56f16d0aee9402ef972e6ef95064
Sven <chanck.mds@gmail.com>
2023-10-27 17:23:43 +0200

    ex 37 is done

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -1,10 +1,10 @@
 export function MouseClicker() {
     const handleClick = (event) => {
-        console.log(event.target.name)
+        console.log(event.currentTarget.name)
     }
     return(
         <div>
-            <button onClick={handleClick} name = 'one'>Click</button>
+            <button onClick={handleClick} name = 'one'><img width="30px" height='30px' /> Click</button>
         </div>
     )
 }
