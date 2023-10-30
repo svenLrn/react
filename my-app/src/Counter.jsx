69520a70262521f8d2326b653a8c8c273980e406
Sven <chanck.mds@gmail.com>
2023-10-31 00:32:09 +0100

    ex 52 is done

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 export function Counter({initialValue = 0, increment = 1}) {
     const [counter, setCounter] = useState(initialValue);
+    const upDownRef = useRef(null);
+    useEffect(() => {
+        const direction =counter > initialValue ? "up" : counter < initialValue ? "down" : "unchanged";
+        if (upDownRef.current !== direction && direction !== "unchanged") {
+            upDownRef.current = direction;
+            console.log("Direction:", upDownRef.current);
+            
+          }
+      }, [counter, initialValue]);
+
+
+    
     function handleCount() {
         setCounter((c)=> c+increment);
     }
