a0b78726b646b9227275d1e717d6c9cf05dd6262
Sven <chanck.mds@gmail.com>
2023-11-08 23:07:07 +0100

    done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,18 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
+import { Navigation } from "./Navigation";
 
 
 
 
 export function App(){
     return(
+        <div>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Welcome name="John" age={30}/>} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="/users/:username" element={<ShowGithubUser />} />
+            </Routes>
+        </div>
         
-        <Routes>
-            <Route path="/" element={<Welcome name="John" age={30}/>} />
-            <Route path="/counter" element={<Counter />} />
-            <Route path="/users/:username" element={<ShowGithubUser />} />
-        </Routes>
         
     )
 }
--- my-app/src/Navigation.jsx
+++ my-app/src/Navigation.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 export function Navigation () {
     return (
         <div >
+            <Link className="navigation" to="/">Welcome</Link>
             <Link className="navigation" to="/counter">Counter</Link>
-            <Link className="navigation" to="/githubuser">GitHubUser</Link>
+            <Link className="navigation" to="/users/first">Users</Link>
+            {/* <Link className="navigation" to="/githubuser">GitHubUser</Link>
             <Link className="navigation" to="/profile">Profile</Link>
-            <Link className="navigation" to="/home">Home</Link>
+            <Link className="navigation" to="/home">Home</Link> */}
         </div>
         )
 }
