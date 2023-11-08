e93f87e4f4e7cd3b1c936b231a17b592d8330643
Sven <chanck.mds@gmail.com>
2023-11-08 18:36:18 +0100

    done

---
+++ my-app/src/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+
+export function Login({onLogin}){
+
+    const [data, setData] = useState({
+        login: '',
+        password: '',
+        remember: false
+    })
+
+    function handleInput(event){
+        const name = event.target.name;
+        const value = event.target.value
+        const checked = event.target.checked 
+        const type = event.target.type
+
+        setData((d)=>{
+            return {
+                ...data,
+                [name]: type === 'checkbox' ? checked : value
+            }
+        })
+    }
+    function handleReset(event){
+        setData((d)=>{
+            return {
+                ...data,
+                login: '',
+                password: '',
+                remember: false
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault(); 
+        onLogin(); 
+      }
+    return(
+        <div>
+            
+            <form onSubmit={handleSubmit}>
+                <input name="login" type="text" value={data.login} onChange={handleInput} />
+                <input name="password" type="password" value={data.password} onChange={handleInput}/>
+                <input name="remember" type="checkbox" checked ={data.remember} onChange={handleInput}/>
+                <button type="submit" disabled={!data.login || !data.password}>Login</button>
+                <button type="button" onClick={handleReset}>Reset</button>
+            </form>
+        </div>
+    )
+}
