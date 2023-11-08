9ddb577874b8461984a12caaa7333b5b6bd529b2
Sven <chanck.mds@gmail.com>
2023-11-08 23:18:35 +0100

    dobe

--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,9 +1,25 @@
-import useSWR from 'swr';
+import { useEffect, useState } from "react"
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
 export function useGithubUser (username) {
-    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
 
+    const [error, setError] = useState(null)
+    const [data, setData] = useState([])
+    async function fetchData(){
+        setError(null)
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}`)
+            const json = await response.json()
+            setData(json)
+            
+
+
+        } catch (error) {
+            setError(error)
+        }
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
 
     return {
         user: data,
