ee033b74066944b07b2f2463d72253053eb762db
Sven <chanck.mds@gmail.com>
2023-11-08 23:55:57 +0100

    done

--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -2,6 +2,13 @@ import useSWR from 'swr';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 export function useGithubUser (username) {
+  if (!username) {
+    return {
+      user: null,
+      isLoading: false,
+      isError: null,
+    };
+  }
     const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
 
 
