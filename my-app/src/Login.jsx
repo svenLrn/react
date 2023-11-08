ef63f11dcd2573d392a3a92bf88b1dcc4ae3efbf
Sven <chanck.mds@gmail.com>
2023-11-09 00:23:01 +0100

    done

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -1,53 +1,49 @@
 import { useState } from "react"
 
-export function Login({onLogin}){
+export function Login({ onLogin }) {
 
-    const [data, setData] = useState({
+    const initialFormData = {
         login: '',
         password: '',
         remember: false
-    })
+    };
 
-    function handleInput(event){
+    const [formData, setFormData] = useState(initialFormData);
+
+    function handleInput(event) {
         const name = event.target.name;
-        const value = event.target.value
-        const checked = event.target.checked // using event or FormData for example:
-                                            // const formData = new FormData(event.target);
-                                            // const name = formData.get("login");
-                                            // const value = formData.get("password");
-        const type = event.target.type
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
 
-        setData((d)=>{
-            return {
-                ...data,
-                [name]: type === 'checkbox' ? checked : value
-            }
-        })
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }));
     }
-    function handleReset(event){
-        setData((d)=>{
-            return {
-                ...data,
-                login: '',
-                password: '',
-                remember: false
-            }
-        })
+
+    function handleReset(event) {
+        setFormData(initialFormData);
     }
 
     function handleSubmit(event) {
-        event.preventDefault(); 
-        onLogin(); 
-      }
-    return(
+        event.preventDefault();
+
+        
+        const formDataObject = new FormData();
+        formDataObject.append("login", formData.login);
+        formDataObject.append("password", formData.password);
+        formDataObject.append("remember", formData.remember);
+
+        
+        onLogin(formDataObject);
+    }
+    // Big disadvantage is limited control, advantage is that it's easy to use
+    return (
         <div>
-            {/* FormData and event.target.elements.namedItem('').value are standard API.
-             not standart are using querySelector or event.target.''.value */}
             <form onSubmit={handleSubmit}>
-                <input name="login" type="text" value={data.login} onChange={handleInput} />
-                <input name="password" type="password" value={data.password} onChange={handleInput}/>
-                <input name="remember" type="checkbox" checked ={data.remember} onChange={handleInput}/>
-                <button type="submit" disabled={!data.login || !data.password}>Login</button>
+                <input name="login" type="text" value={formData.login} onChange={handleInput} />
+                <input name="password" type="password" value={formData.password} onChange={handleInput} />
+                <input name="remember" type="checkbox" checked={formData.remember} onChange={handleInput} />
+                <button type="submit" disabled={!formData.login || !formData.password}>Login</button>
                 <button type="button" onClick={handleReset}>Reset</button>
             </form>
         </div>
