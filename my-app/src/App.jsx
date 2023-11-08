af2609db4619ef2370b6e62590efbad8c802e48a
Sven <chanck.mds@gmail.com>
2023-11-08 22:45:45 +0100

    done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,5 @@
-import { Colors } from "./Colors";
-import { Container } from "./Container";
-import { LanguageProvider } from "./LanguageContext";
-import { Login } from "./Login";
-import { TodoList } from "./TodoList";
+import { Routes, Route } from "react-router-dom";
 import { Welcome } from "./Welcome";
-import { Clock } from "./Clock";
-
-import { GitHubUsers } from "./GitHubUsers";
-import { UserLocation } from "./UserLocation";
 
 
 
@@ -15,22 +7,9 @@ import { UserLocation } from "./UserLocation";
 export function App(){
     return(
         
-        <div>
-            <Colors colors={[
-                { id: 1, name: 'red' },
-                { id: 2, name: 'green' },
-                { id: 3, name: 'blue' },
-            ]}/>
-            
-            <Welcome name={'John'} age={30}/>
-            
-            <Login onLogin={()=> console.log('Logined')}/>
-            <Container title="Container Title"><TodoList/></Container>
-            <LanguageProvider>
-                <Clock/>
-            </LanguageProvider>
-            <UserLocation/>
-        </div>
+        <Routes>
+            <Route path="/" element={<Welcome name="John" age={30}/>} />
+        </Routes>
         
     )
 }
